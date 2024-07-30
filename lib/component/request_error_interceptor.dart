2a24751ecbeea5ae76fca896ef77dcdd6110700f
nondirectional@yeesun.com <1059170150@qq.com>
2024-07-31 01:17:26 +0800

    优化请求错误处理逻辑并增强用户体验
    通过去除冗余的超时错误处理代码，并对状态码错误处理进行集中管理，简化了错误处理逻辑。此外，通过在发生错误时使用SnackBarReminder展示错误信息，提升了用户的交互体验。同时，确保在请求取消和其他异常情况下，错误能够被正确处理和传递。BREAKING CHANGE: 移除了对连接超时、发送超时和接收超时的单独处理逻辑，现在统一在默认错误处理中进行处理。如果依赖于这些特定的超时错误处理，请更新代码以适应新的错误处理方式。

--- lib/component/request_error_interceptor.dart
+++ lib/component/request_error_interceptor.dart
@@ -1,52 +1,42 @@
+
 import 'package:dio/dio.dart';
+import 'package:non_memos/component/snack_bar_reminder.dart';
+import 'package:non_memos/key/global_keys.dart';
 
 class RequestErrorInterceptor extends Interceptor {
+
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
     // 处理错误
     switch (err.type) {
-      case DioExceptionType.connectionTimeout:
-        // 连接超时
-        print("连接超时");
-        break;
-      case DioExceptionType.sendTimeout:
-        // 发送超时
-        print("发送超时");
-        break;
-      case DioExceptionType.receiveTimeout:
-        // 接收超时
-        print("接收超时");
-        break;
       case DioExceptionType.badResponse:
-        // 根据状态码处理
+      // 根据状态码处理
         switch (err.response?.statusCode) {
           case 400:
-            // 处理400错误
-            print("400");
-            break;
-          case 401:
-            // 处理401错误
-            print("401");
-            break;
-          // 其他状态码...
+            SnackBarReminder.showSnackBarRemindByScaffoldMessengerState(
+                GlobalKeys.scaffoldMessengerKey, RemindType.error,
+                "Request fail,message: ${err.response?.data['message']}",
+                const Duration(milliseconds: 1500));
+        break;
+        // 其他状态码...
           default:
-            print(err.response?.statusCode);
+            SnackBarReminder.showSnackBarRemindByScaffoldMessengerState(
+                GlobalKeys.scaffoldMessengerKey, RemindType.error,
+                "Request fail,message: $err.message",
+                const Duration(milliseconds: 1500));
         }
         break;
-      case DioExceptionType.cancel:
-        // 请求取消
-        break;
       default:
-        // 默认错误处理
+      // 默认错误处理
         break;
     }
 
-    // 可以在这里统一显示错误提示
-    // showErrorToast(err.message);
-    if(err.response != null){
+
+    if (err.response != null) {
       handler.resolve(err.response!);
+    } else {
+      // 继续传递错误
+      // handler.next(err);
     }
-    // 继续传递错误
-    // handler.next(err);
   }
 }
