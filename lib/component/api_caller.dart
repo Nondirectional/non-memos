64b4f2eeefb1f4042cc4c48277827a720d23f9a8
nondirectional@yeesun.com <1059170150@qq.com>
2024-07-31 01:17:04 +0800

    添加请求URI管理并优化请求配置
    引入`RequestUris`管理请求路径，使代码更清晰易维护。同时，调整请求配置，移除注释掉的CookieManager添加语句，并确保请求错误拦截器正确添加。此外，优化了`signin`方法的请求URI使用方式，提升了代码的可读性和可维护性。

--- lib/component/api_caller.dart
+++ lib/component/api_caller.dart
@@ -2,6 +2,7 @@ import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:non_memos/component/request_error_interceptor.dart';
+import 'package:non_memos/component/request_uris.dart';
 
 class ApiCaller {
   static const String _host = "http://memos.w-wbr.space";
@@ -14,13 +15,13 @@ class ApiCaller {
       connectTimeout: const Duration(seconds: 5),
       receiveTimeout: const Duration(seconds: 5),
     ));
-    // _instance?.interceptors.add(CookieManager(_cookieJar));
+    _instance?.interceptors.add(CookieManager(_cookieJar));
     _instance?.interceptors.add(RequestErrorInterceptor());
   }
 
   Future<Response>? signin(String username, String password, bool neverExpire) {
     return _instance?.post(
-      "/api/v1/auth/signin",
+      RequestUris.signin.uri,
       queryParameters: {
         "username": username,
         "password": password,
